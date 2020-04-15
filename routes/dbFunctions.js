93361f17d95c65b168fdd44a370e9ce3d7f1ec65
omario <o.khandxb@gmail.com>
2020-04-15 19:25:46 -0400

    consoles

--- routes/dbFunctions.js
+++ routes/dbFunctions.js
@@ -3,8 +3,10 @@ var shelterModel = require('../models/Shelter.js');
 let router = require('express').Router();
 
 router.get('/', async (req, res) => {
+  console.log('awdawdawdawd')
   const shelters = await shelterModel.find({"OCCUPANCY_DATE":"2020-01-03T00:00:00"});
   try {
+    console.log('TEDWEDQW')
     res.send(shelters);
   } catch (err) {
     res.status(500).send(err);
