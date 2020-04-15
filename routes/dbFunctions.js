76f83e18adf171951c7aa6519af86f3f396153bf
omario <o.khandxb@gmail.com>
2020-04-15 19:27:55 -0400

    query change

--- routes/dbFunctions.js
+++ routes/dbFunctions.js
@@ -4,7 +4,7 @@ let router = require('express').Router();
 
 router.get('/', async (req, res) => {
   console.log('awdawdawdawd')
-  const shelters = await shelterModel.find({"OCCUPANCY_DATE":"2020-01-03T00:00:00"});
+  const shelters = await shelterModel.find({"OCCUPANCY_DATE":"2020-04-10T00:00:00"});
   try {
     console.log('TEDWEDQW')
     res.send(shelters);
